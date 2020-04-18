9ab60be61d3e117a7a76cee963f557ac22516e4a
Jukka Hopeavuori <jukka.hopea@gmail.com>
2020-04-19 01:44:13 +0300

    Fix cors for dev environment

--- backend/src/utils/cors.js
+++ backend/src/utils/cors.js
@@ -1,7 +1,7 @@
 const getCorsHeaders = () => {
   // eslint-disable-next-line no-undef
   const { STAGE, FRONTEND_URL } = process.env
-  const origin = STAGE === 'dev' ? '*' : FRONTEND_URL
+  const origin = STAGE === 'local' ? '*' : FRONTEND_URL
   return {
     'Access-Control-Allow-Origin': origin,
     'Access-Control-Allow-Credentials': true,
