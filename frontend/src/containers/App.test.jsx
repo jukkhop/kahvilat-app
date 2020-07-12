331949dd06f1715a8ff9320deb50a1e4e88e9ae2
Jukka Hopeavuori <jukka.hopea@gmail.com>
2020-07-12 14:06:23 +0300

    Update frontend test

--- frontend/src/containers/App.test.jsx
+++ frontend/src/containers/App.test.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
-test('renders landing page title', () => {
+it('should render the landing page title', () => {
   const { getByText } = render(<App />)
   const linkElement = getByText(/Varhainen versio nyt kokeiltavissa./i)
   expect(linkElement).toBeInTheDocument()
