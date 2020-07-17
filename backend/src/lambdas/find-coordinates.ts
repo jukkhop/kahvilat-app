a3f6bda8044fca444befc2400986484062499617
Jukka Hopeavuori <jukka.hopea@gmail.com>
2020-07-17 16:15:20 +0300

    Improve and refactor find-coordinates lambda

--- backend/src/lambdas/find-coordinates.ts
+++ backend/src/lambdas/find-coordinates.ts
@@ -1,19 +1,29 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import Cache from '../cache'
 import GoogleClient from '../clients/google-client'
-import { cachedFetch, checkQueryStringParameters, getCorsHeaders } from '../utils'
+import { cachedFetch, checkQueryStringParameters, mkErrorResponse, mkResponse } from '../utils'
 
-export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
   const { GOOGLE_API_KEY = '', REDIS_HOST = '', REDIS_PORT = '' } = process.env
-  const queryParams = event.queryStringParameters || {}
-
-  checkQueryStringParameters(Object.keys(queryParams), ['address'])
-
-  const { address } = queryParams
   const cache = new Cache(REDIS_HOST, Number(REDIS_PORT))
   const client = new GoogleClient(GOOGLE_API_KEY, 'fi')
+  return await helper(event, cache, client)
+}
 
-  const [status, response] = await cachedFetch(
+async function helper(
+  event: APIGatewayProxyEvent,
+  cache: Cache,
+  client: GoogleClient,
+): Promise<APIGatewayProxyResult> {
+  const queryParams = event.queryStringParameters || {}
+  const validationErrors = checkQueryStringParameters(Object.keys(queryParams), ['address'])
+
+  if (validationErrors.length > 0) {
+    return mkErrorResponse(400, validationErrors)
+  }
+
+  const { address } = queryParams
+  const [status, body] = await cachedFetch(
     cache,
     'find-coordinates',
     { address },
@@ -21,9 +31,7 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
     //
   )
 
-  return {
-    statusCode: status,
-    headers: getCorsHeaders(),
-    body: response,
-  }
+  return mkResponse(status, body)
 }
+
+export { handler, helper }
