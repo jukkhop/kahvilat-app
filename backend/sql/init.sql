2c62b9bd987fdb5a85221300b17d38e6028ca8d4
Jukka Hopeavuori <jukka.hopea@gmail.com>
2021-03-07 13:24:38 +0200

    Remove leftover sql files

--- backend/sql/init.sql
+++
@@ -1,9 +0,0 @@
-\c kahvilat_db
-
-CREATE TABLE places(
-  "id" serial,
-  "external_id" VARCHAR(255) NOT NULL,
-  "name" VARCHAR(255) NOT NULL
-);
-
-INSERT INTO places ("external_id", "name") VALUES ('kaffaroastery', 'Kaffa Roastery');
