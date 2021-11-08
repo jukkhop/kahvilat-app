3b392ea7ab753aae7ad787fdb9c71db6ac67cbdf
Jukka Hopeavuori <jukka.hopea@gmail.com>
2021-11-08 23:19:17 +0200

    Use bare awscli for deploying frontend to s3

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -18,19 +18,11 @@ source "../environment/${env}-variables.env"
 
 set +o allexport
 
+bucket="${TF_VAR_app_subdomain_name}"
+
 export AWS_ACCESS_KEY_ID="${TF_VAR_aws_access_key}"
 export AWS_SECRET_ACCESS_KEY="${TF_VAR_aws_secret_key}"
 export AWS_DEFAULT_REGION="${TF_VAR_aws_region}"
 
-source "../environment/scripts/aws-utils.sh"
-
-bucket_name="${TF_VAR_app_subdomain_name}"
-distribution_id="$(get_cloudfront_distribution_id ${bucket_name})"
-
-s3deploy \
-  -source=build/ \
-  -region=${AWS_DEFAULT_REGION} \
-  -key=${AWS_ACCESS_KEY_ID} \
-  -secret=${AWS_SECRET_ACCESS_KEY} \
-  -distribution-id=${distribution_id} \
-  -bucket=${bucket_name}
+aws s3 rm --recursive --quiet "s3://${bucket}"
+aws s3 sync build/ "s3://${bucket}"
